6dfaf07c285d429b528c6477e98a38c687110880
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-10-09 15:05:10 +0200

    Further improved javascript fetch function and edited index.html.

--- resources/javascript/indexScript.js
+++ resources/javascript/indexScript.js
@@ -1,21 +1,28 @@
 getMovies();
 let movies= []
 
-const URLkommuner = "http://localhost:8080/movies"
-const URLkommune  = "http://localhost:8080/movie"
-const URLregioner  = "http://localhost:8080/regioner"
-function getMovies(){
-    fetch('http://localhost:8080/getAllMovie')
-        .then(result => {
-            if (result.status >= 400) {
-                throw new Error("Server returned an error");
-            }
-            return result.json();
-        }).then(body => {
-            movies = body
 
-        }).
-    catch(error => {
-        console.error("There was an error fetching the data:", error);
+function getMovies(){
+    fetch("http://localhost:8080/Kino_XP/index")
+    .then(response => response.json())
+    .then(data => {
+        const movieList = document.getElementById("movieList");
+        data.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.className = "movie-card";
+
+            const moviePoster = document.createElement("img");
+            moviePoster.src = movie.posterUrl;
+            movieCard.appendChild(moviePoster);
+
+            const movieTitle = document.createElement("h2");
+            movieTitle.textContent = movie.title;
+            movieCard.appendChild(movieTitle);
+
+            movieList.appendChild(movieCard);
+        });
+    })
+    .catch(error => {
+        console.error("An error occurred:", error);
     });
 }
