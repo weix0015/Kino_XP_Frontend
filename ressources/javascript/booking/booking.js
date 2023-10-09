9ceec0943d75bd3d32ab85011c28410f9dc847b6
weix0015 <weix0015@stud.kea.dk>
2023-10-09 14:18:21 +0200

    added packages for better structure

--- ressources/javascript/booking/booking.js
+++ ressources/javascript/booking/booking.js
@@ -1,16 +1,12 @@
-// Get the seat container
 const seatContainer = document.getElementById('seat-container');
 
-// Define the number of rows and seats
 const numRows = 20;
 const numSeatsPerRow = 14;
 
-// Loop to create the rows
 for (let row = 0; row < numRows; row++) {
   const rowElement = document.createElement('div');
   rowElement.className = 'row';
 
-  // Loop to create the seats in each row
   for (let seat = 0; seat < numSeatsPerRow; seat++) {
     const square = document.createElement('i');
     square.className = 'fa-solid fa-square';
@@ -20,12 +16,10 @@ for (let row = 0; row < numRows; row++) {
   seatContainer.appendChild(rowElement);
 }
 
-// Function to toggle the color of a square
 function toggleSeatColor(event) {
   event.target.classList.toggle('blue');
 }
 
-// Add a click event listener to all seats
 const seats = document.querySelectorAll('.fa-square');
 seats.forEach(seat => {
   seat.addEventListener('click', toggleSeatColor);
