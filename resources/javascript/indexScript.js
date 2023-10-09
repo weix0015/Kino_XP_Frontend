479160d3bf7817d1dfedb8eda58410a87e52e377
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-10-09 11:15:20 +0200

    Added a getMovies() method to javascript.

--- resources/javascript/indexScript.js
+++ resources/javascript/indexScript.js
@@ -1,4 +1,21 @@
+getMovies();
+let movies= []
 
+const URLkommuner = "http://localhost:8080/movies"
+const URLkommune  = "http://localhost:8080/movie"
+const URLregioner  = "http://localhost:8080/regioner"
 function getMovies(){
-    fetch()
+    fetch('http://localhost:8080/getAllMovie')
+        .then(result => {
+            if (result.status >= 400) {
+                throw new Error("Server returned an error");
+            }
+            return result.json();
+        }).then(body => {
+            movies = body
+
+        }).
+    catch(error => {
+        console.error("There was an error fetching the data:", error);
+    });
 }
