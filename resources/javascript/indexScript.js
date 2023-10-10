5829bf1182cd41f7502289313d19054f7d5253f3
Geargrind <lass347s@stud.kea.dk>
2023-10-10 14:10:22 +0200

    Made changes to the index.html and indexScript.js, got a functional fetch, with posters, sourced from out database and web.

--- resources/javascript/indexScript.js
+++ resources/javascript/indexScript.js
@@ -1,11 +1,14 @@
-getMovies();
-let movies= []
-getMoviePoster()
 
-function getMovies(){
-    fetch("http://localhost:8080/index")
-    .then(response => response.json())
-    .then(data => {
+let movies= []
+getMovies()
+
+
+
+async function getMovies() {
+    try {
+        const response = await fetch("http://localhost:8080/movies"); // Erstat med den rigtige backend-rute
+        const data = await response.json();
+
         const movieList = document.getElementById("movieList");
         data.forEach(movie => {
             const movieCard = document.createElement("div");
@@ -13,12 +16,110 @@ function getMovies(){
 
             const moviePoster = document.createElement("img");
             moviePoster.src = movie.posterUrl;
+            moviePoster.className = "movie-poster";
             movieCard.appendChild(moviePoster);
 
             const movieTitle = document.createElement("h2");
             movieTitle.textContent = movie.title;
             movieCard.appendChild(movieTitle);
 
+            const showtimesContainer = document.createElement("div");
+            showtimesContainer.className = "showtimes-container";
+
+            // Hent tidspunkter fra viewing-objekterne for denne film
+            movie.viewings.forEach(viewing => {
+                const showtimeButton = document.createElement("button");
+                showtimeButton.className = "time-button";
+                showtimeButton.textContent = viewing.showTime;
+                showtimeButton.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    // Her kan du håndtere, hvad der skal ske, når en tid klikkes, f.eks. omdirigere til booking siden
+                });
+                showtimesContainer.appendChild(showtimeButton);
+            });
+
+            movieCard.appendChild(showtimesContainer);
+            movieList.appendChild(movieCard);
+        });
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+
+/*
+async function getMovies() {
+    try {
+        const response = await fetch("http://localhost:8080/movies");
+        const data = await response.json();
+
+        const movieList = document.getElementById("movieList");
+        data.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.className = "movie-card";
+
+            const moviePoster = document.createElement("img");
+            moviePoster.src = movie.posterUrl;
+            moviePoster.className = "movie-poster";
+            movieCard.appendChild(moviePoster);
+
+            const movieTitle = document.createElement("h2");
+            movieTitle.textContent = movie.title;
+            movieTitle.className = "movie-headline";
+            movieCard.appendChild(movieTitle);
+            const showtimesContainer = document.createElement("div");
+            showtimesContainer.className = "showtimes-container";
+            movie.showtimes.forEach(showtime => {
+                const showtimeButton = document.createElement("button");
+                showtimeButton.className = "time-button";
+                showtimeButton.textContent = showtime;
+                showtimeButton.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    // Her kan du håndtere, hvad der skal ske, når en tid klikkes, f.eks. omdirigere til booking siden
+                });
+                
+            });
+            showtimesContainer.appendChild(showtimeButton);
+            movieList.appendChild(movieCard);
+            
+        });
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+ */
+
+// Find alle tidspunkt-knapper
+
+
+// Løb igennem hver knap og tilføj en klikhåndterer
+
+
+
+/*
+function getMovies(){
+    fetch("http://localhost:8080/movies")
+    .then(response => response.json())
+    .then(data => {
+        const movieList = document.getElementById("movieList");
+        data.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.className = "movie-card";
+            
+            if(movie.poster && movie.title){
+            const moviePoster = document.createElement("img");
+            moviePoster.src = movie.poster;
+            movieCard.appendChild(moviePoster);
+
+            const movieTitle = document.createElement("h2");
+            movieTitle.textContent = movie.title;
+            movieCard.appendChild(movieTitle);
+            }
+            else{
+                console.log("Couldn't find movie")
+            }
+            
             movieList.appendChild(movieCard);
         });
     })
@@ -41,4 +142,5 @@ function getMoviePoster(title) {
         .catch(error => {
             console.error("An error occurred:", error);
         });
-}
+} */
+
